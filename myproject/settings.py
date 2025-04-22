cf41e71aef180c01f478ae344967308493a91e1c
ViuRemix/ <worksor.78@gmail.com>
2025-04-23 00:09:48 +0700

    fix ALLOWED_HOSTS

--- myproject/settings.py
+++ myproject/settings.py
@@ -32,8 +32,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True").lower() == "true"
 
-# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(" ")
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(" ")
+# ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 # Security settings
 # DEBUG = False  # Comment out this line
