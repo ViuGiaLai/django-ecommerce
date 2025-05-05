f77b179c664c00392e0708dcc2436a93a2f05444
ViuRemix <152360526+ViuRemix@users.noreply.github.com>
2025-05-05 21:45:27 +0700

    Update settings.py ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1,viugialai.site,www.viugialai.site").split(",")

--- myproject/settings.py
+++ myproject/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(" ")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1,viugialai.site,www.viugialai.site").split(",")
 # ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 # Security settings
